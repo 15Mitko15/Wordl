df86098fb3e95131c0fb3df6e98fe4bcb5506f32
Dimitar Stoilov <dmstoilov02@gmail.com>
2024-02-06 15:32:29 +0200

    Add the colors

---
+++ colors.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class Color(Enum):
+    # Colors indicateing the state of a letter
+    GREEN = 1 # Indicates that the letter is in it right possition
+    YELLOW = 2 # Indicates that the letters is in the word but not in the right possition
+    GERY = 3 # Indicates that the letter is not in the word
